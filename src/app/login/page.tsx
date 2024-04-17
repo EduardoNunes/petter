b89c859aee9c729176d431aa385652ae8f82c64c
Eduardo Lago Nunes <eduardolagonunes@gmail.com>
2024-04-17 20:49:00 -0300

    feat: initiated login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import Petter from "@/components/Petter/Petter";
+
+export default function Login() {
+  return (
+    <>
+      <div>
+        <Petter />
+      </div>
+      <p>Vamos nos conectar?</p>
+    </>
+  );
+}
---
+++ src/components/Petter/Petter.tsx
@@ -0,0 +1,16 @@
+export default function Petter() {
+  return (
+    <>
+      <div className="flex items-center relative flex-col w-[200px]">
+        <div>
+          <span className="text-extraLarge text-verdeForteSombra">P</span>
+          <span className="text-extraLarge text-azulEscuro">e</span>
+          <span className="text-extraLarge text-rosaForteSombra">T</span>
+          <span className="text-extraLarge text-azulForteSombra">T</span>
+          <span className="text-extraLarge text-amareloPadrao">e</span>
+          <span className="text-extraLarge text-rosaForte">R</span>
+        </div>
+      </div>
+    </>
+  );
+}
