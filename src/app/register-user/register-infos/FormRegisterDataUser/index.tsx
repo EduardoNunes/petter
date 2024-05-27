7dedc89a80271273be4cdacb743b8d5308b153d3
Eduardo <eduardolagonunes@gmail.com>
2024-05-27 18:10:26 -0300

    feat: validations date and cep Ok

--- src/app/register-user/register-infos/FormRegisterDataUser/index.tsx
+++ src/app/register-user/register-infos/FormRegisterDataUser/index.tsx
@@ -2,31 +2,71 @@ import Button from "@/components/Button/Button";
 import ErrorWindow from "@/components/Error/ErrorWindown";
 import Input from "@/components/Input/Input";
 import { Label } from "@/components/Label/Label";
+import Loading from "@/components/Loading/Loading";
 import Select from "@/components/Select/Select";
 import { useStepContext } from "@/context/useStepContext";
+import api from "@/server/api";
 import viaCep from "@/server/api-viacep";
 import formatCep from "@/utils/formatCEP";
 
 import { schemaRegisterInfosUser } from "@/validation/schemaRegisterInfosUser";
-import { ChangeEvent, useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { ChangeEvent, SyntheticEvent, useEffect, useState } from "react";
 
 export default function FormUserRegisterData() {
-  const { handleToAddCurrentStep } = useStepContext();
+  const router = useRouter();
   const [selectedOption, setSelectOption] = useState("");
   const [date, setDate] = useState("");
   const [cep, setCep] = useState("");
-  const [addressFrom, setAddressForm] = useState("");
+  const [gender, setGender] = useState("");
   const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  async function handleClickGoOn(event: SyntheticEvent) {
+    event.preventDefault();
+
+    try {
+      setLoading(true);
+
+      await schemaRegisterInfosUser.validate(
+        {
+          date,
+          cep,
+          gender,
+        },
+        { abortEarly: false }
+      );
+
+      const response = await api.post("/users-register-infos", {
+        date,
+        cep,
+        gender,
+      });
+
+      console.log((await response).status, "RESPONSE");
+
+      if ((await response).status === 201) {
+        router.push("");
+      }
+    } catch (error: any) {
+      if (
+        error.response &&
+        error.response.data &&
+        error.response.data.message
+      ) {
+        setError(error.response.data.message);
+      } else if (error.errors && error.errors.length > 0) {
+        setError(error.errors[0]);
+      } else {
+        setError(error.message || "Ocorreu um erro.");
+      }
+    } finally {
+      setLoading(false);
+    }
+  }
 
   const handleChangeData = async (event: ChangeEvent<HTMLInputElement>) => {
-    const newData = event.target.value;
-    try {
-      await schemaRegisterInfosUser.validate({ date: newData });
-      setDate(newData);
-    } catch (validationError: any) {
-      setError(validationError.errors[0]);
-      console.log("ERROR", validationError.errors[0]);
-    }
+    setDate(event.target.value);
   };
 
   const handleChangeAddress = async (event: ChangeEvent<HTMLInputElement>) => {
@@ -52,8 +92,9 @@ export default function FormUserRegisterData() {
   }, [cep]);
 
   return (
-    <form className="h-[100%] mb-6 mt-2" onSubmit={handleToAddCurrentStep}>
+    <form className="h-[100%] mb-6 mt-2" onSubmit={handleClickGoOn}>
       {error && <ErrorWindow textError={error} setError={setError} />}
+      {loading && <Loading />}
       <div className="overflow-y-auto" style={{ height: "100% - [120px]" }}>
         <div className="mb-3">
           <Label labelHtmlFor="birth">Sua data de nascimento</Label>
--- src/validation/schemaRegisterInfosUser.tsx
+++ src/validation/schemaRegisterInfosUser.tsx
@@ -9,7 +9,12 @@ export interface InfosUser {
 export const schemaRegisterInfosUser = yup.object().shape({
   date: yup
     .date()
+    .required("Data é obrigatória")
     .typeError("A data fornecida não é uma data válida.")
     .max(new Date(), "A data não pode ser maior que a data atual")
-    .required("Data é obrigatória")
+    .min(new Date("1900-01-01"), "A data deve ser de uma pessoa viva."),
+  cep: yup
+    .string()
+    .required("O CEP é obrigatório.")
+    .matches(/^\d{5}-\d{3}$/, "CEP deve estar no formato XXXXX-XXX"),
 });
