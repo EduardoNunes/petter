28680dd448bf7a1f0d71cd696f69ff7ed2ee9772
Eduardo <eduardolagonunes@gmail.com>
2024-06-07 13:46:32 -0300

    feat: show 10 cards on timeline working, need to pass for the cards on exibition page

--- src/app/(routesApp)/home/Content/Components/Card.tsx/Card.tsx
+++ src/app/(routesApp)/home/Content/Components/Card.tsx/Card.tsx
@@ -1,8 +1,28 @@
+"use client";
+
 import FooterCard from "./FooterCard/FooterCard";
 import dataCardTemp from "../../../../../../../public/dataTemp/dataCardsTemp";
-
+import { useEffect, useState } from "react";
+import api from "@/server/api";
 
 export default function Card() {
+  const [imageSrc, setImageSrc] = useState("");
+
+  useEffect(() => {
+    async function loadTimeline() {
+      console.log("TESTE");
+      try {
+        const response = await api.get("show-card-timeline/top-10-images", {});
+
+        console.log("RESPONSE", response.data);
+      } catch (error) {
+        console.log("ERRO", error);
+      }
+    }
+
+    loadTimeline();
+  }, []);
+
   return (
     <div className="flex flex-col relative w-full h-full overflow-auto">
       {dataCardTemp.map((card, index) => (
