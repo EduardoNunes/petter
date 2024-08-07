9a661dac725786b2683a5544cb89bffab66f5c06
Eduardo <eduardolagonunes@gmail.com>
2024-08-07 12:28:40 -0300

    FEAT(next-auth.d TYPES): adicionado opcional ? em todos os itens p funcionar

--- src/types/next-auth.d.tsx
+++ src/types/next-auth.d.tsx
@@ -3,13 +3,13 @@ import NextAuth from "next-auth/next";
 declare module "next-auth" {
   interface Session {
     user: {
-      accessToken: string;
-      id: number;
-      name: string;
-      email: string;
-      profileImage: string;
-      petterInfo: [];
-      userInfo: {};
+      accessToken?: string;
+      id?: number;
+      name?: string;
+      email?: string;
+      profileImage?: string;
+      petterInfo?: [];
+      userInfo?: {};
     };
   }
 }
