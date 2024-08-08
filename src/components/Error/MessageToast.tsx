1da25f857dab5f35572dd687319a8c374a625068
Eduardo <eduardolagonunes@gmail.com>
2024-08-08 07:04:50 -0300

    FIX(MessageTiast): resolvido erro de n ter bg color no toast

--- src/components/Error/MessageToast.tsx
+++ src/components/Error/MessageToast.tsx
@@ -7,14 +7,7 @@ interface MessageToastProps {
 
 const MessageToast: React.FC<MessageToastProps> = ({ textError, setToast }) => {
   const [isVisible, setIsVisible] = useState(true);
-  const [bgColor, setBgColor] = useState("amareloPadrao");
   const sucesso = textError.includes("sucesso");
-
-  useEffect(() => {
-    if (sucesso === true) {
-      setBgColor("verdeForte");
-    }
-  }, []);
 
   const hideMessageToast = () => {
     setTimeout(() => {
@@ -34,7 +27,9 @@ const MessageToast: React.FC<MessageToastProps> = ({ textError, setToast }) => {
   return (
     <div className="absolute top-[50%] translate-x-[-50%] left-[50%] translate-y-[-50%] z-10">
       <div
-        className={`flex items-center text-center h-auto w-auto px-4 py-4 rounded-lg z-1 bg-${bgColor}`}
+        className={`flex items-center text-center h-auto w-auto px-4 py-4 rounded-lg z-1 bg-${
+          sucesso ? "verdePastel" : "amareloPadrao"
+        }`}
       >
         <p className="font-secondary font-bold">{textError}</p>
       </div>
