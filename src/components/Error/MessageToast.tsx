91edb841a2a294594365428fbce00fc758662127
Eduardo <eduardolagonunes@gmail.com>
2024-08-08 09:02:27 -0300

    FIX(messageToast): consertado bug de bg-color n aparecer

--- src/components/Error/MessageToast.tsx
+++ src/components/Error/MessageToast.tsx
@@ -27,8 +27,8 @@ const MessageToast: React.FC<MessageToastProps> = ({ textError, setToast }) => {
   return (
     <div className="absolute top-[50%] translate-x-[-50%] left-[50%] translate-y-[-50%] z-10">
       <div
-        className={`flex items-center text-center h-auto w-auto px-4 py-4 rounded-lg z-1 bg-${
-          sucesso ? "verdePastel" : "amareloPadrao"
+        className={`flex items-center text-center h-auto w-auto px-4 py-4 rounded-lg z-1 ${
+          sucesso ? "bg-verdePastel" : "bg-amareloPadrao"
         }`}
       >
         <p className="font-secondary font-bold">{textError}</p>
