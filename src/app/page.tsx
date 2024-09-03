c44c87c8e1a24913eecadf69a64000d54e14ed35
Eduardo <eduardolagonunes@gmail.com>
2024-09-03 19:50:56 -0300

    FEAT: reduzido tempo da tela inicial de 3 p 2 s

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Opening() {
   useEffect(() => {
     setTimeout(() => {
       router.push("/login");
-    }, 3000);
+    }, 2000);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
