5f4aa5cc57b9f7329eda612033f381702258f755
Eduardo <eduardolagonunes@gmail.com>
2024-09-04 08:20:02 -0300

    STYLE: agora sim

--- src/app/(routes-app)/registers/register-infos/FormRegisterDataUser/index.tsx
+++ src/app/(routes-app)/registers/register-infos/FormRegisterDataUser/index.tsx
@@ -124,7 +124,7 @@ export default function FormUserRegisterData() {
 
   return (
     <form
-      className="flex flex-col justify-between h-full my-6"
+      className="flex flex-col justify-between h-full mt-6"
       style={{ height: "calc(100% - 68px)" }}
       onSubmit={onSubmit}
     >
