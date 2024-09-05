c4329c384d3735cc39dd9f2653f439a7c2267b14
Eduardo <eduardolagonunes@gmail.com>
2024-09-05 12:31:26 -0300

    FEAT: adicionado temporizador para logout para n travar na home page por questão de espera do await

--- src/app/(routes-app)/pages/profile/HeaderProfile/index.tsx
+++ src/app/(routes-app)/pages/profile/HeaderProfile/index.tsx
@@ -1,24 +1,32 @@
+import Loading from "@/components/Loading/Loading";
 import { signOut } from "next-auth/react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 interface HeaderProfileProps {
   petterName: string;
 }
 
 export default function HeaderProfile({ petterName }: HeaderProfileProps) {
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
 
   async function logout() {
+    setLoading(true);
+
     await signOut({
       redirect: false,
     });
 
-    router.replace("/login");
+    setTimeout(() => {
+      router.replace("/login");
+    }, 3000);
   }
 
   return (
     <div className="absolute top-2 flex items-center justify-between h-[7%] w-[90%]">
+      {loading && <Loading />}
       <div>
         <h1 className="text-extraLarge1">{petterName}</h1>
       </div>
