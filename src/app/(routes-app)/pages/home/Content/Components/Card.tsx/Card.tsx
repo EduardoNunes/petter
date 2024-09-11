f6ff70f46d2a04fd3a08772d3e6c573e2fc1be37
Eduardo <eduardolagonunes@gmail.com>
2024-09-10 21:07:59 -0300

    FEAT: adicionado scroll infinito na timeline

--- src/app/(routes-app)/pages/home/Content/Components/Card.tsx/Card.tsx
+++ src/app/(routes-app)/pages/home/Content/Components/Card.tsx/Card.tsx
@@ -7,7 +7,7 @@ import { useTimeLineContext } from "@/context/timeLineContext";
 import api from "@/server/api";
 import { getSession } from "next-auth/react";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import FooterCard from "./FooterCard/FooterCard";
 import HeaderCard from "./HeaderCard/HeaderCard";
 
@@ -27,46 +27,78 @@ interface PetterInfo {
 }
 
 export default function Card() {
-  const {
-    likesCount,
-    likesCountId,
-    commentsCount,
-    likedByMe,
-  } = useTimeLineContext();
+  const { likesCount, likesCountId, commentsCount, likedByMe } =
+    useTimeLineContext();
   const { setLoading } = useSelfContext();
   const [imageSrc, setImageSrc] = useState<ImageType[]>([]);
   const [toast, setToast] = useState("");
   const [petterLoggedId, setPetterLoggedId] = useState<number | undefined>(
     undefined
   );
+  const scrollableDivRef = useRef<HTMLDivElement>(null);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
-    async function loadTimeline() {
-      const session = await getSession();
-      const token = session?.user.accessToken;
-      const petterInfoId = session?.user.petterInfo as PetterInfo[];
+    loadTimeline(page);
+  }, [page, likesCount, likesCountId, commentsCount, likedByMe]);
 
-      setPetterLoggedId(petterInfoId[0].id);
+  const loadTimeline = async (newPage: number) => {
+    const session = await getSession();
+    const token = session?.user.accessToken;
+    const petterInfoId = session?.user.petterInfo as PetterInfo[];
 
-      try {
-        const response = await api.get("show-card-timeline/top-10-images", {
+    setPetterLoggedId(petterInfoId[0].id);
+    console.log("CHAMOU");
+    try {
+      const response = await api.get(
+        `show-card-timeline/images?page=${newPage}`,
+        {
           headers: { Authorization: `Bearer ${token}` },
-        });
-        const images = response.data.top10ImagesWithCounts;
-
-        setImageSrc(images);
-      } catch (error: any) {
-        const response = errorResponse(error);
-        setLoading(false);
-        setToast(response);
-      }
+        }
+      );
+      const images = response.data.imagesWithCounts;
+      console.log("IMAGES", images);
+      setImageSrc((prevImages) => [...prevImages, ...images]);
+    } catch (error: any) {
+      const response = errorResponse(error);
       setLoading(false);
+      setToast(response);
     }
-    loadTimeline();
-  }, [likesCount, likesCountId, commentsCount, likedByMe]);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (scrollableDivRef.current) {
+        const scrollDiv = scrollableDivRef.current;
+        let isBottom =
+          scrollDiv.scrollTop + scrollDiv.clientHeight ===
+          scrollDiv.scrollHeight;
+
+        if (isBottom) {
+          setPage((prevPage) => prevPage + 1);
+        }
+      }
+    };
+
+    const divElement = scrollableDivRef.current;
+
+    if (divElement) {
+      divElement.addEventListener("scroll", handleScroll);
+    }
+
+    return () => {
+      if (divElement) {
+        divElement.removeEventListener("scroll", handleScroll);
+      }
+    };
+  }, []);
 
   return (
-    <div className="flex flex-col w-full h-full overflow-auto">
+    <div
+      className="flex flex-col w-full h-full overflow-auto"
+      ref={scrollableDivRef}
+    >
       {toast && <MessageToast textError={toast} setToast={setToast} />}
       {!imageSrc || imageSrc.length === 0
         ? ""
@@ -90,7 +122,11 @@ export default function Card() {
                 descriptionCard={image.description}
                 likesCount={image.likesCount}
                 imageId={image.id}
-                likedByMe={image.Like.some((item) => item.petterInfoId === petterLoggedId) || false}
+                likedByMe={
+                  image.Like.some(
+                    (item) => item.petterInfoId === petterLoggedId
+                  ) || false
+                }
               />
             </div>
           ))}
