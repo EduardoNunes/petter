3ef4993b24192efb2a0a034246645913960fba7d
Eduardo <eduardolagonunes@gmail.com>
2024-09-13 14:06:16 -0300

    REFACTOR

--- src/app/(routes-app)/pages/home/Content/Components/Card.tsx/FooterCard/FooterCard.tsx
+++ src/app/(routes-app)/pages/home/Content/Components/Card.tsx/FooterCard/FooterCard.tsx
@@ -19,7 +19,6 @@ const FooterCard: React.FC<FooterCardProps> = ({
 }) => {
   const {
     handleClickLikeFunction,
-    handleClickShowComment,
     setTimelineImageId,
     updateCommentsCount,
     updateCommentCountId,
