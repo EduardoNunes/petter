a3ccf99a4ec59c68cd778ca3575c36c7f9818d76
Eduardo <eduardolagonunes@gmail.com>
2024-09-17 13:16:46 -0300

    FIX: teste de ajuste de inicialização do app sem erro de carregamento

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,70 +1,73 @@
 "use client";
 
-import Logo from "@/components/Logo/Logo";
-import Image from "next/image";
-import "./animation.css";
-import { useEffect } from "react";
+import PetterBlack from "@/components/Petter/PetterBlack";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import "./animation.css";
 
 export default function Opening() {
   const router = useRouter();
 
   useEffect(() => {
-/*     setTimeout(() => {
-      console.log("AQUI") */
+    const redirect = () => {
+      console.log("Navigating to /login");
       router.push("/login");
-/*     }, 2000); */
-  }, []);
+    };
+
+    redirect();
+  }, [router]);
 
   return (
     <>
-      <div className="flex flex-col items-center justify-center h-[100vh] w-[100vw] bg-gradient-to-r from-azulPalido to-branco">
-        <Logo />
-        <div className="flex gap-2 pt-14">
-          <Image
-            src="/images/dog.png"
-            width={30}
-            height={30}
-            alt="Dog"
-            className="animation1"
-          />
-          <Image
-            src="/images/cat.png"
-            width={30}
-            height={30}
-            alt="Cat"
-            className="animation2"
-          />
-          <Image
-            src="/images/duck.png"
-            width={30}
-            height={30}
-            alt="Duck"
-            className="animation3"
-          />
-          <Image
-            src="/images/hamster.png"
-            width={30}
-            height={30}
-            alt="Hamster"
-            className="animation4"
-          />
-          <Image
-            src="/images/turttle.png"
-            width={30}
-            height={30}
-            alt="Turttle"
-            className="animation5"
-          />
-          <Image
-            src="/images/fish.png"
-            width={30}
-            height={30}
-            alt="Fish"
-            className="animation6"
-          />
+      {
+        <div className="flex flex-col items-center justify-center h-[100vh] w-[100vw]">
+          <PetterBlack fontSize={"extraLarge"} />
+          {/* <div className="flex gap-2 pt-14">
+            <Image
+              src="/images/dog.png"
+              width={30}
+              height={30}
+              alt="Dog"
+              className="animation1"
+            />
+            <Image
+              src="/images/cat.png"
+              width={30}
+              height={30}
+              alt="Cat"
+              className="animation2"
+            />
+            <Image
+              src="/images/duck.png"
+              width={30}
+              height={30}
+              alt="Duck"
+              className="animation3"
+            />
+            <Image
+              src="/images/hamster.png"
+              width={30}
+              height={30}
+              alt="Hamster"
+              className="animation4"
+            />
+            <Image
+              src="/images/turttle.png"
+              width={30}
+              height={30}
+              alt="Turttle"
+              className="animation5"
+            />
+            <Image
+              src="/images/fish.png"
+              width={30}
+              height={30}
+              alt="Fish"
+              className="animation6"
+            />
+          </div> */}
         </div>
-      </div>
+      }
     </>
   );
 }
