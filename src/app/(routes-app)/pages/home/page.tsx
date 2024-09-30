6932df97aa6eb4c80a7fd5af6cd825b0a03c1201
Eduardo <eduardolagonunes@gmail.com>
2024-09-30 15:36:49 -0300

    REFACTOR: retirado tudo o que não era utilizado da homepage

--- src/app/(routes-app)/pages/home/page.tsx
+++ src/app/(routes-app)/pages/home/page.tsx
@@ -1,34 +1,28 @@
 "use client";
 
+import Loading from "@/components/Loading/Loading";
 import ModalComment from "@/components/Modals/ModalComment/ModalComment";
+import ModalSearch from "@/components/Modals/ModalSearch/ModalSearch";
+import { useHomeContext } from "@/context/homeContext";
+import { useSelfContext } from "@/context/selfContext";
+import { useTimeLineContext } from "@/context/timeLineContext";
+import { useQuery } from "react-query";
 import Footer from "../../../../components/Footer/Footer";
 import ContentHome from "./Content/Content";
 import HeaderHome from "./HeaderHome/HeaderHome";
-import { useTimeLineContext } from "@/context/timeLineContext";
-import { useSelfContext } from "@/context/selfContext";
-import { useEffect } from "react";
-import Loading from "@/components/Loading/Loading";
-import { useHomeContext } from "@/context/homeContext";
-import ModalSearch from "@/components/Modals/ModalSearch/ModalSearch";
 
 export default function Home() {
   const { commentsOpenModal } = useTimeLineContext();
   const { isOpenSearch } = useHomeContext();
-  const { getSelf, loading, setLoading } = useSelfContext();
+  const { getSelf } = useSelfContext();
 
-  useEffect(() => {
-    setLoading(false);
-  }, []);
-
-  useEffect(() => {
-    getSelf();
-  }, []);
+  const { isLoading } = useQuery("self", getSelf);
 
   return (
     <div className="flex flex-col w-[90%] h-full">
       {commentsOpenModal && <ModalComment />}
       {isOpenSearch && <ModalSearch />}
-      {loading && <Loading />}
+      {isLoading && <Loading />}
 
       <HeaderHome />
       <div className="w-full h-[calc(100%-124px)]">
